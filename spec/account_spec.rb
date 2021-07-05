40e241138d44eeddfd6b3de126c8f51e4bcc5df1
Jenny <jennybell42@gmail.com>
2021-07-05 16:33:52 +0100

    bank account initializes with balance of 0

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,7 +3,9 @@
 require 'account'
 
 describe Account do
-  it 'makes ana ccount with a balance of zero' do
-    expect(subject.balance).to eq(0)
+  describe '#initialize' do
+    it 'makes ana ccount with a balance of zero' do
+      expect(subject.balance).to eq(0)
+    end
   end
 end
