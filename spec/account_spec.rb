72af6273d242f16614dca817e23ab59dbf2c381b
Jenny <jennybell42@gmail.com>
2021-07-08 17:27:20 +0100

    tests refactored - DRY

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -11,16 +11,14 @@ describe Account do
 
   describe '#deposit' do
     it 'stores a credit transaction' do
-      account = Account.new
-      account.deposit(5)
+      subject.deposit(5)
 
-      expect(account.transactions.length).to eq(1)
+      expect(subject.transactions.length).to eq(1)
     end
 
-    it 'allows money to be deposited into the bank account' do
-      account = Account.new
-      account.deposit(5)
-      expect(account.balance).to eq(5)
+    it 'allows money to be deposited into the bank subject' do
+      subject.deposit(5)
+      expect(subject.balance).to eq(5)
     end
 
     it 'raises an error if deposit amount is zero' do
@@ -33,33 +31,30 @@ describe Account do
   end
 
   describe '#withdraw' do
-    it 'stores a debit transaction' do
-      account = Account.new
-      account.deposit(5)
-      account.withdraw(2)
-
-      expect(account.transactions.length).to eq(2)
+    before(:each) do
+      subject.deposit(5)
     end
 
-    it 'allows money to be withdrawn from the bank account' do
-      account = Account.new
-      account.deposit(5)
-      account.withdraw(3)
-      expect(account.balance).to eq(2)
+    it 'stores a debit transaction' do
+      subject.withdraw(2)
+
+      expect(subject.transactions.length).to eq(2)
+    end
+
+    it 'allows money to be withdrawn from the bank subject' do
+      subject.withdraw(3)
+      expect(subject.balance).to eq(2)
     end
 
     it 'raises an error if requested withdrawal exceeds balance' do
-      subject.deposit(5)
       expect { subject.withdraw(7) }.to raise_error "Withdrawal request exceeds account balance of #{subject.balance}"
     end
 
     it 'raises an error if withdrawal amount is zero' do
-      subject.deposit(5)
       expect { subject.withdraw(0) }.to raise_error 'Invalid transaction'
     end
 
     it 'raises an error if withdrawal amount is negative' do
-      subject.deposit(5)
       expect { subject.withdraw(-2) }.to raise_error 'Invalid transaction'
     end
   end
